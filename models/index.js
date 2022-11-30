52166f3de85ccaef45e35a709ee97f045b213e9d
David Milton <davidmilton@Davids-MacBook-Pro.local>
2022-11-30 09:16:35 -0800

    deleted console.logs

--- models/index.js
+++ models/index.js
@@ -9,18 +9,7 @@ mongoose.connect(
     { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
-// CONSOLE.LOG() CONNECTION STATUS //
-mongoose.connection.on('connected', () => {
-    console.log('mongoose connected to ', connectionString);
-});
 
-mongoose.connection.on('disconnected', () => {
-    console.log('mongoose disconnected to ', connectionString);
-});
-
-mongoose.connection.on('error', (error) => {
-    console.log('mongoose error ', error);
-});
 
 // ACCESS MODELS //
 module.exports.Book = require("./book.js");
